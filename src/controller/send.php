3bd1105ba526d1d5d299eea5aeefa390337280c7
setWrong <rothenbaecher.dennis@gmail.com>
2015-08-28 08:00:32 +0200

    send controller

---
+++ src/controller/send.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: rothe
+ * Date: 28.08.2015
+ * Time: 07:51
+ */
+namespace hpay\controller;
+
+class Send{
+
+    public static function curlRequest(){
+
+    }
+
+    public static function xmlRequest(){
+
+
+    }
+}
